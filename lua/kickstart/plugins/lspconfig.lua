d109118971a41d3a9f06219f203795181e7e18ba
Neil Castelino <neil.fl93@gmail.com>
2025-07-19 18:37:52 -0400

    enable vtsls

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -219,6 +219,19 @@ return {
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {
+          settings = {
+            typescript = {
+              -- https://code.visualstudio.com/docs/typescript/typescript-editing#_inlay-hints
+              inlayHints = {
+                enumMemberValues = { enabled = true },
+                functionLikeReturnTypes = { enabled = true },
+                parameterNames = { enabled = 'literals' },
+                parameterTypes = { enabled = true },
+                propertyDeclarationTypes = { enabled = true },
+                variableTypes = { enabled = true },
+              },
+            },
+          },
           init_options = {
             plugins = {
               {
@@ -235,7 +248,67 @@ return {
             'vue',
           },
         },
-        vue_ls = {},
+        vtsls = {
+          settings = {
+            vtsls = {
+              autoUseWorkspaceTsdk = true,
+              experimental = {
+                -- Truncate inlay hint
+                -- https://github.com/neovim/neovim/issues/27240
+                maxInlayHintLength = 40,
+                completion = {
+                  -- Execute fuzzy match of completion items on server side. Enable this
+                  -- will help filter out useless completion items from tsserver
+                  enableServerSideFuzzyMatch = true,
+                },
+              },
+              tsserver = {
+                globalPlugins = {
+                  {
+                    name = '@vue/typescript-plugin',
+                    location = '',
+                    languages = { 'vue' },
+                    configNamespace = 'typescript',
+                  },
+                },
+              },
+            },
+          },
+          filetypes = {
+            'typescript',
+            'javascript',
+            'javascriptreact',
+            'typescriptreact',
+            'vue',
+          },
+        },
+        vue_ls = {
+          on_init = function(client)
+            client.handlers['tsserver/request'] = function(_, result, context)
+              local clients = vim.lsp.get_clients { bufnr = context.bufnr, name = 'vtsls' }
+              if #clients == 0 then
+                vim.notify('Could not find `vtsls` lsp client, `vue_ls` would not work without it.', vim.log.levels.ERROR)
+                return
+              end
+              local ts_client = clients[1]
+
+              local param = unpack(result)
+              local id, command, payload = unpack(param)
+              ts_client:exec_cmd({
+                title = 'vue_request_forward', -- You can give title anything as it's used to represent a command in the UI, `:h Client:exec_cmd`
+                command = 'typescript.tsserverRequest',
+                arguments = {
+                  command,
+                  payload,
+                },
+              }, { bufnr = context.bufnr }, function(_, r)
+                local response_data = { { id, r.body } }
+                ---@diagnostic disable-next-line: param-type-mismatch
+                client:notify('tsserver/response', response_data)
+              end)
+            end
+          end,
+        },
         eslint = {},
         --
 
@@ -288,6 +361,11 @@ return {
           end,
         },
       }
+
+      -- https://github.com/vuejs/language-tools/wiki/Neovim
+      vim.lsp.config('vtsls', servers.vtsls)
+      vim.lsp.config('vue_ls', servers.vue_ls)
+      vim.lsp.enable { 'vtsls', 'vue_ls' }
     end,
   },
 }
